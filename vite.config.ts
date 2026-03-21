7e878460c46a367ad51f4b56a5922275636448f0
Đỗ Đức Anh <anhdott1@gmail.com>
2026-03-21 18:57:32 +0700

    update for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+// 1. Thêm dòng import nitro này
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
@@ -13,6 +15,10 @@ const config = defineConfig({
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
+    
+    // 2. Kích hoạt nitro plugin tại đây
+    nitro(),
+    
     viteReact(),
   ],
 })
